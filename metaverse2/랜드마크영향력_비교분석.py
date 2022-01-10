c8417922a551bf93798bf2f3ed6e8725fdac7078
minanalysis <94737570+minanalysis@users.noreply.github.com>
2022-01-10 18:02:54 +0900

    Update 랜드마크영향력_비교분석.py

--- metaverse2/랜드마크영향력_비교분석.py
+++ metaverse2/랜드마크영향력_비교분석.py
@@ -1,10 +1,12 @@
-# # 산,강 제외한 구별 타일당 가격
+## 산,강 제외한 구별 타일당 가격 EDA 
 
-# 확정 랜드마크 산, 강 불러오기 
+## 확정 랜드마크 산, 강 불러오기 
+## df_mr_nowland로 파생변수 생성. 
+
 df_mr_nowland=pd.read_csv("finaldata/(확정랜드 구)산,강.csv", encoding='cp949')
-df_mr_nowland
-
 df_mr_nowland.rename(columns={'Unnamed: 0':'구'})
+
+##meta_nowland_gu(확정랜드마크 포함 구별 데이터프레임)에 필요한 데이터 병합해주기
 
 meta_nowland_gu['산강제외_타일수']=meta_nowland_gu['타일수변환']-df_mr_nowland['타일수']
 
@@ -12,7 +14,7 @@ meta_nowland_gu['산강제외_시장가TP_원']=meta_nowland_gu['시장가TP_원
 
 meta_nowland_gu['산강제외_타일당가격']=meta_nowland_gu['산강제외_시장가TP_원']/meta_nowland_gu['산강제외_타일수']
 
-meta_nowland_gu
+
 #메타버스 내의 기본(아무것도 제외안함)타일수 및 시장가 & 산강제외 타일수 및 시장가 
 
 # # 산, 강, 랜드마크 제외한 구별 타일당 가격
